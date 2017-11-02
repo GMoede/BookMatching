0df57426ee726d9c2f4dd8a6194dd254f5963572
Griffin Moede <griffinmoede@gmail.com>
2017-11-01 21:37:18 -0700

    Fixed Formatting

--- createLoginTables.sql
+++ createLoginTables.sql
@@ -1,19 +1,19 @@
 CREATE TABLE ProjectUser (
-	username varchar(50) NOT NULL UNIQUE, 
-	password varchar(200) NOT NULL, 
-	usertype varchar(20)
+  username varchar(50) NOT NULL UNIQUE, 
+  password varchar(200) NOT NULL, 
+  usertype varchar(20)
 );
 
 
 CREATE TABLE Book(
-	title varchar(100),
-	firstname varchar(50),
-	lastname varchar(50),
-	copyright int, 
-	lexile int, 
-	pages int, 
-	recommended varchar(1),
-	topic varchar(50),
-	prot_feat varchar(50), 
-	prot_gender varchar(50)
+  title varchar(100),
+  firstname varchar(50),
+  lastname varchar(50),
+  copyright int, 
+  lexile int, 
+  pages int, 
+  recommended varchar(1),
+  topic varchar(50),
+  prot_feat varchar(50), 
+  prot_gender varchar(50)
 );
