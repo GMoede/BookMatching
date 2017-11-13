8a5c2ef4e522ce3f6ddeabf9382983fb9938b38b
sbooth1 <sbooth1@scu.edu>
2017-11-13 14:47:24 -0800

    verify input data is correct

--- studentportal.js
+++ studentportal.js
@@ -1,38 +1,39 @@
 var prev_search = [];
 
-function updateSearch(){
+function update(){
 	//first I will check to make sure the input is correct 
 
 	var errors = [];
-/*
-	if(document.getElementById("copyright").value === "" || document.getElementById("copyright").value.length < 4 || document.getElementById("copyright").value.length >= 5 || isNaN(document.getElementById("copyright").value)){
-		errors.push("Copyright");
+
+	if(document.getElementById("input_firstname").value === ""){
+		errors.push("Author First Name");
 	}
-	if(document.getElementById("lexile").value === ""){
+	if(document.getElementById("input_lastname").value === ""){
+		errors.push("Author Last Name");
+	}
+	if(document.getElementById("input_lexile").value === ""){
 		errors.push("Lexile");
 	}
-	if(document.getElementById("pages").value === "" || isNaN(document.getElementById("pages").value)){
+	if(document.getElementById("input_pages").value === "" || isNaN(document.getElementById("input_pages").value)){
 		errors.push("Page Count");
 	}
 	//checking lexile validity
-	var lexile = document.getElementById("lexile").value;
-	lexile.replace(/L/g, ''); 
+	var lexile = document.getElementById("input_lexile").value;
+	lexile = lexile.replace(/L/g, ''); 
 	if(isNaN(lexile) && errors.indexOf("Lexile") < 0){
 		errors.push("Lexile");
 	}
-*/
+
 	if(errors.length > 0){
 		alert("The following fields are incomplete/incorrect: " + errors);
 	} else {
 		//at this point we know it is valid data so we will save it so it can be used on the next search
 		setTimeout(function(){ 
-			prev_search.push(document.getElementById("topic").value);
-			prev_search.push(document.getElementById("protag1").value);
-			prev_search.push(document.getElementById("protag2").value);
-			prev_search.push(document.getElementById("protag3").value);
-			prev_search.push(document.getElementById("copyright").value);
-			prev_search.push(document.getElementById("lexile").value);
-			prev_search.push(document.getElementById("pages").value); }, 3000);
+			prev_search.push(document.getElementById("input_topic").value);
+			prev_search.push(document.getElementById("input_prot_feat").value);
+			prev_search.push(document.getElementById("input_prot_gender").value);
+			prev_search.push(document.getElementById("input_lexile").value);
+			prev_search.push(document.getElementById("input_pages").value); }, 3000);
 
 		console.log("Previous Search Criteria: " + prev_search);
 
