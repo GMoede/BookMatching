2ba2a9756549b1443d42758170cf344f9870387f
sbooth1 <sbooth1@scu.edu>
2017-11-13 14:48:45 -0800

    update naming conventions to match updated html

--- teacherportal.js
+++ teacherportal.js
@@ -1,30 +1,32 @@
 function verifyInputs(){
 		var errors = [];
-		if (document.getElementById("title").value === ""){
+		if (document.getElementById("input_title").value === ""){
 			errors.push("Title");
 		}
-		if(document.getElementById("first_name").value === ""){
+		if(document.getElementById("input_firstname").value === ""){
 			errors.push("Author First Name");
 		}
-		if(document.getElementById("last_name").value === ""){
+		if(document.getElementById("input_lastname").value === ""){
 			errors.push("Author Last Name");
 		}
-		if(document.getElementById("copyright").value === "" || document.getElementById("copyright").value.length < 4 || document.getElementById("copyright").value.length >= 5 || isNaN(document.getElementById("copyright").value)){
+		if(document.getElementById("input_copyright").value === "" || document.getElementById("input_copyright").value.length < 4 || document.getElementById("input_copyright").value.length >= 5 || isNaN(document.getElementById("input_copyright").value)){
 			errors.push("Copyright");
 		}
-		if(document.getElementById("lexile").value === ""){
+		if(document.getElementById("input_lexile").value === ""){
 			errors.push("Lexile");
 		}
-		if(document.getElementById("pages").value === "" || isNaN(document.getElementById("pages").value)){
+		if(document.getElementById("input_pages").value === "" || isNaN(document.getElementById("input_pages").value)){
 			errors.push("Page Count");
 		}
 		//checking lexile validity
-		var lexile = document.getElementById("lexile").value;
-		lexile.replace(/L/g, ''); 
+		var lexile = document.getElementById("input_lexile").value;
+		lexile = lexile.replace(/L/g, ''); 
 		if(isNaN(lexile) && errors.indexOf("Lexile") < 0){
 			errors.push("Lexile");
 		}
 		if(errors.length > 0){
 			alert("The following fields are incomplete/incorrect: " + errors);
+		} else{
+			//add book to database (AJAX call to php file)
 		}
 }
