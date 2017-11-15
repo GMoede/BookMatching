1c0a2661718c9213ce36a49ba824caeedbf5e0d6
Griffin Moede <griffinmoede@gmail.com>
2017-11-14 20:38:25 -0800

    Working Search

--- studentportal.js
+++ studentportal.js
@@ -28,14 +28,22 @@ function update(){
 		alert("The following fields are incomplete/incorrect: " + errors);
 	} else {
 		//at this point we know it is valid data so we will save it so it can be used on the next search
-		setTimeout(function(){ 
-			prev_search.push(document.getElementById("input_topic").value);
-			prev_search.push(document.getElementById("input_prot_feat").value);
-			prev_search.push(document.getElementById("input_prot_gender").value);
-			prev_search.push(document.getElementById("input_lexile").value);
-			prev_search.push(document.getElementById("input_pages").value); }, 3000);
+		  url = "application.php"
 
-		console.log("Previous Search Criteria: " + prev_search);
+
+  //send data using post with element id lexile
+		  var posting = $.post( url, { input_lexile: $('#input_lexile').val(), input_firstname: $('#input_firstname').val(), input_lastname: $('#input_lastname').val(), input_pages: $('#input_pages').val(), input_topic: $('#input_topic').val(), input_prot_feat: $('#input_prot_feat').val(), input_prot_gender: $('#input_prot_gender').val() } );
+
+  //alerts the results
+		  posting.done(function( data ) {
+    //alert(data);
+		  if(data) {
+		    $('#search_results').html(data);
+		  }
+		  else{
+		    alert('failure');
+		  }
+		  });
 
 	}
 }
@@ -44,3 +52,5 @@ function classSearch(){
 	//document.getElementById("test").innerHTML = "TEST TEST";
 	document.getElementById("search_results").innerHTML += "<div class=\"col-md-7\" id=\"book_entry\" style=\"border-color: black;\"><div class=\"col-md-3\"><p class=\"search_text\"> Insert photo here </p></div><div class=\"col-md-3\"><p class=\"search_text\"> Book Title </p><p class=\"search_text\"> Author </p><p class=\"search_text\"> Copyright </p></div><div class=\"col-md-3 text-right\"><p class=\"search_text\"> protag1 </p><p class=\"search_text\"> protag2 </p><p class=\"search_text\"> protag3 </p></div></div> <br>";
 }
+
+
